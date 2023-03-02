e0e6101bdd0b06554410b1ae8574beb70d635097
Abdullah Tariq Latif <abdullahtariqlatif@gmail.com>
2023-03-02 17:22:23 +0500

    Added Model Validations

--- app/controllers/modal_dashboards_controller.rb
+++ app/controllers/modal_dashboards_controller.rb
@@ -22,10 +22,27 @@ class ModalDashboardsController < ApplicationController
   end
 
   def create_employment
+    # Create a new Employment object with the given parameters
+    @employment = Employment.new(employment_params)
+
+    # Attempt to save the new employment record to the database
+    if @employment.save
+      respond_to do |format|
+        format.js { render json: { status: :created, message: 'Record saved successfully', user: @employment } }
+      end
+    else
+      respond_to do |format|
+        format.js { render json: { status: :unprocessable_entity, errors: @employment.errors } }
+      end
+    end
   end
 
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :nick_name, :email, :phone_number)
+    end
+
+    def employment_params
+      params.require(:employment).permit(:employer_name, :employment_started_date, :employment_ended_date)
     end
 end
---
+++ app/javascript/controllers/employment_controller.js
@@ -0,0 +1,6 @@
+// app/javascript/controllers/employment_controller.js
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = [ ]
+}
