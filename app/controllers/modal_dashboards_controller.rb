953bf64bef29bb1b135bba9d221c5b691339cbe1
Abdullah Tariq Latif <abdullahtariqlatif@gmail.com>
2023-03-03 03:31:32 +0500

    Implemented Employement Requirements, Expect for each next filed being enabled and the previous respective fileds being disabled if the current field value is false

--- app/controllers/modal_dashboards_controller.rb
+++ app/controllers/modal_dashboards_controller.rb
@@ -22,27 +22,44 @@ class ModalDashboardsController < ApplicationController
   end
 
   def create_employment
-    # Create a new Employment object with the given parameters
-    @employment = Employment.new(employment_params)
+    ActiveRecord::Base.transaction do
+      responses = params[:forms].map do |form_params|
+                    # Create or update a record using the form parameters
+                    @employment = Employment.find_or_initialize_by(id: form_params[:id])
+                    if @employment.update(employment_params(form_params))
+                      { status: :created, message: 'Record saved successfully', employment: @employment }
+                    else
+                      { status: :unprocessable_entity, errors: @employment.errors }
+                    end
+                  end
 
-    # Attempt to save the new employment record to the database
-    if @employment.save
-      respond_to do |format|
-        format.js { render json: { status: :created, message: 'Record saved successfully', user: @employment } }
-      end
-    else
-      respond_to do |format|
-        format.js { render json: { status: :unprocessable_entity, errors: @employment.errors } }
+      # Check if any responses are errors
+      if responses.any? { |response| response[:status] == :unprocessable_entity }
+        render json: responses, status: :unprocessable_entity
+      else
+        render json: responses
       end
     end
   end
+
+  def employment_form
+    render partial: 'modal_dashboards/partials/employment_form'
+  end
+
+  # def initial_employment_form
+  #   render partial: 'modal_dashboards/partials/initial_employment_form'
+  # end
 
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :nick_name, :email, :phone_number)
     end
 
-    def employment_params
-      params.require(:employment).permit(:employer_name, :employment_started_date, :employment_ended_date)
+    # def employment_params
+    #   params.require(:employment).permit(:employer_name, :employment_started_date, :employment_ended_date)
+    # end
+
+    def employment_params(form_params)
+      form_params.permit(:employer_name, :employment_started_date, :employment_ended_date)
     end
 end
--- app/javascript/controllers/employment_controller.js
+++ app/javascript/controllers/employment_controller.js
@@ -2,5 +2,89 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = [ ]
+  static targets = ["employerName", "startDate", "endDate", "submitAllBtn", "employmentModal", "employmentModalFooter"]
+
+  connect() {
+    // this.loadEmploymentForm();
+    // this.startDateTarget.disabled = true
+    // this.startDateTarget.classList.add("bg-gray-300")
+
+    // this.endDateTarget.disabled = true
+    // this.endDateTarget.classList.add("bg-gray-300")
+  }
+
+  validateName(event) {
+    const regex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/
+    const element = event.currentTarget;
+
+    if (element.value.length === 0 || element.value === '' || !regex.test(element.value)) {
+      // name is invalid
+      this.disableSubmitAllBtn();
+      this.setErrorForInput(element.nextElementSibling, 'Invalid First Name');
+
+      return false;
+    } else {
+      // name is valid
+      this.removeErrorForInput(element.nextElementSibling);
+      this.enableSubmitAllBtn();
+
+      return true;
+    }
+  }
+
+  // Submit all forms function # 1
+  submitAllForms() {
+    $('#employmentModal form').each(function(index, element) {
+      $.ajax({
+        url: $(element).attr('action'),
+        method: 'POST',
+        data: $(element).serialize(),
+        success: function(data) {
+          console.log(data);
+        },
+        error: function(xhr, status, error) {
+          console.log(xhr.responseText);
+          console.log(status);
+          console.log(error);
+        }
+      });
+    });
+  }
+
+  addNewEmploymentForm() {
+    fetch('/employment_form')
+    .then(response => response.text())
+    .then(html => {
+      this.employmentModalFooterTarget.insertAdjacentHTML('beforebegin', html);
+    })
+  }
+
+  disableSubmitAllBtn() {
+    this.submitAllBtnTarget.disabled = true;
+    this.submitAllBtnTarget.classList.add("bg-gray-300")
+  }
+
+  enableSubmitAllBtn() {
+    this.submitAllBtnTarget.disabled = false;
+    this.submitAllBtnTarget.classList.remove("bg-gray-300")
+  }
+
+  setErrorForInput(input, message) {
+    input.innerText = message;
+    input.classList.add('error-message');
+  }
+
+  removeErrorForInput(input) {
+    input.innerText = '';
+    input.classList.remove('error-message');
+  }
+
+
+  // loadEmploymentForm() {
+  //   fetch('/initial_employment_form')
+  //   .then(response => response.text())
+  //   .then(html => {
+  //     this.employmentModalTarget.innerHTML = html;
+  //   })
+  // }
 }
